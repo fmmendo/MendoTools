414f372f8de24bb3cd3b9002b9fabeb8512f8d75
Filipe Duarte <me@fmendo.com>
2015-08-23 10:17:22 +0100

    rating control size

--- Mendo.UAP/Controls/RatingControl.cs
+++ Mendo.UAP/Controls/RatingControl.cs
@@ -28,5 +28,33 @@ namespace Mendo.UAP.Controls
         // Using a DependencyProperty as the backing store for Rating.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty RatingProperty =
             DependencyProperty.Register("Rating", typeof(Double), typeof(RatingControl), new PropertyMetadata(0d));
+
+
+
+        public string FullGlyph
+        {
+            get { return (string)GetValue(FullGlyphProperty); }
+            set { SetValue(FullGlyphProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for FullGlyph.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FullGlyphProperty =
+            DependencyProperty.Register("FullGlyph", typeof(string), typeof(RatingControl), new PropertyMetadata("&#xE1CF;"));
+
+
+
+        public string EmptyGlyph
+        {
+            get { return (string)GetValue(EmptyGlyphProperty); }
+            set { SetValue(EmptyGlyphProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for EmptyGlyph.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty EmptyGlyphProperty =
+            DependencyProperty.Register("EmptyGlyph", typeof(string), typeof(RatingControl), new PropertyMetadata("&#xE1CF;"));
+
+
+
+
     }
 }
